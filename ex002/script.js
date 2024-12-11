322dc14a2435554aa7d313bb93153818f54bb45c
Thaís Cristine Coelho <tcristine84@gmail.com>
2024-12-11 15:51:36 +0000

    Projeto finalizado com imagens e cores de fundo modificadas

--- ex002/script.js
+++ ex002/script.js
@@ -1,1 +1,20 @@
-alert('Olá!');
+function carregar(){
+    var msg = window.document.getElementById('msg');
+    var img = window.document.getElementById('manha');
+    var data = new Date();
+    var hora = data.getHours();
+    //var hora = 20 //Teste de hora forçado
+    msg.innerHTML = `Agora são ${hora} horas.`;
+
+    //Mostrando a imagem correta
+    if(hora >= 0 && hora < 12){
+        img.src = 'imagens/manha.png';
+        document.body.style.background = '#B0BC98';
+    } else if (hora >= 12 && hora < 18){
+        img.src = 'imagens/tarde.png';
+        document.body.style.background = '#D496A7';
+    } else {
+        img.src = 'imagens/noite.png';
+        document.body.style.background = '#5D576B';
+    }        
+}
